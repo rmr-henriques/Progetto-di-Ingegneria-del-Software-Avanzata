7bcb5237b91db5014e02c42993b853f4dd8b0fb3
Ineslbr <il.rocha@campus.fct.unl.pt>
2022-07-08 18:22:05 +0200

    up to date

---
+++ BlackjackGame/src/main/java/Blackjack/App.java
@@ -0,0 +1,13 @@
+package Blackjack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
