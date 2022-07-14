559c99399376e7fcd187a51b06dca1e65dabaf5c
rmr-henriques <rmr.henriques@campus.fct.unl.pt>
2022-07-14 21:08:15 +0200

    gonna make the game wooooork

--- BlackjackGame/src/main/java/Blackjack/FrontEnd/Main.java
+++ BlackjackGame/src/main/java/Blackjack/FrontEnd/Main.java
@@ -1,4 +1,4 @@
-package Blackjack;
+package Blackjack.FrontEnd;
 
 import java.awt.*;
 import javax.swing.*;   
--- BlackjackGame/src/main/java/Blackjack/FrontEnd/Menu.java
+++ BlackjackGame/src/main/java/Blackjack/FrontEnd/Menu.java
@@ -1,4 +1,4 @@
-package Blackjack;
+package Blackjack.FrontEnd;
 
 import java.awt.*;
 import java.awt.event.*;
